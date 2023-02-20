0b347c8590308510d4823bcdfd091d0aca8fd82e
shivani sawarkar <117519427+shivanisawarakar@users.noreply.github.com>
2023-02-20 15:11:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-function minCostToFormRopes(event)
-{
+function minimumCostOfRopes(n, arr) {
+  let cost = 0;
+  const minHeap = [];
 
-   function minCost(event) 
-   {
-	event.preventDefault();
-	var inputElement = document.querySelector('input').value;
-    var arr = inputElement.split(',');
-	arr.sort(function (a,b) {return a-b});
+  for (let i = 0; i < n; i++) {
+    minHeap.push(arr[i]);
+  }
 
-	var cost = 0;
-	var input = document.querySelector("input").value;
-	var arr = input.split(",");
-	var cost=0;
-	while(arr.length > 1){
-		var res =Number(arr[0]) + Number(arr[1]);
-		arr.push(res);
-		cost += res;
-		arr.shift();
-		arr.shift();
-	    arr.sort(function (a,b) {return a-b});
-		arr.sort(function (a,b) {return a-b});
-		var temp=Number(arr[0]) + Number(arr[1]);
-		cost+=temp;
-		arr.push(temp);
-		arr.splice(0,2);
-		arr.sort(function (a,b) {return a-b});
+  while (minHeap.length > 1) {
+    const firstMin = minHeap.shift();
+    const secondMin = minHeap.shift();
+    const newRope = firstMin + secondMin;
+    cost += newRope;
+    minHeap.push(newRope);
+    minHeap.sort((a, b) => a - b);
+  }
 
-	}
-	document.getElementById("result").textContent = cost;
-}
-	document.querySelector("div").textContent=cost;
+  return cost;
 }
 
